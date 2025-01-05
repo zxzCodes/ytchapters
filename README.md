a0ecfb3b8c7224b98c8272995a9230a9a2d2200e
Blackbeard <azkabdiali2@gmail.com>
2025-01-05 16:52:40 -0500

    mod val

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-here visit the project https://ytchapters.vercel.app/
+# Project Overview
+
+Here visit the project [ytchapters](https://ytchapters.vercel.app/)
 
 paste a youtube link and it will generate chapters for you
 
--- utils/validation/index.ts
+++ utils/validation/index.ts
@@ -1,6 +1,7 @@
 "use server";
 
 export async function validateYouTubeLink(link: string): Promise<boolean> {
-  const isYouTubeLink = /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)[\w-]{11}(\S*)?$/;
+  // Regular expression to match various YouTube URL formats, including those with query parameters
+  const isYouTubeLink = /^(https?:\/\/)?(www\.)?(youtube\.com\/(watch\?v=|embed\/|v\/|shorts\/|@[\w-]+\/|channel\/)|youtu\.be\/)[\w-]{11}(\S*)?$/;
   return isYouTubeLink.test(link);
 }
